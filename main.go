68f091b3adbe1fa75e7f4d8ac5a3a3a4d8fac2ba
Devansh Akruvala <61865437+devansh-akruvala@users.noreply.github.com>
2024-04-12 10:31:12 -0400

    creating server and loadbalancer struct and interfaces

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"net/http/httputil"
 	"net/url"
 	"os"
@@ -26,6 +27,26 @@ func handleErr(err error) {
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
+	}
+}
+
+type Server interface {
+	Address() string
+	IsAlive() bool
+	serve(resW http.ResponseWriter, reqW *http.Request)
+}
+
+type LoadBalancer struct {
+	port            string
+	roundRobinCount int
+	servers         []Server
+}
+
+func newLoadBalancer(port string, servers []Server) *LoadBalancer {
+	return &LoadBalancer{
+		port:            port,
+		roundRobinCount: 0,
+		servers:         servers,
 	}
 }
 
