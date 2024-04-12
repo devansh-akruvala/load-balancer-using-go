29bad06ce1a61d9831327fd1493d03ee728a9f42
Devansh Akruvala <61865437+devansh-akruvala@users.noreply.github.com>
2024-04-12 00:52:10 -0400

    creating simple server

--- main.go
+++ main.go
@@ -1,5 +1,34 @@
 package main
 
+import (
+	"fmt"
+	"net/http/httputil"
+	"net/url"
+	"os"
+)
+
+type server struct {
+	address string
+	proxy   *httputil.ReverseProxy
+}
+
+func newServer(addr string) *server {
+	serverUrl, err := url.Parse(addr)
+	handleErr(err)
+
+	return &server{
+		address: addr,
+		proxy:   httputil.NewSingleHostReverseProxy(serverUrl),
+	}
+}
+
+func handleErr(err error) {
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
 func main() {
 
 }
